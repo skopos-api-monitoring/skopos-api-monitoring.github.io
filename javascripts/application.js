f688b05310d1bced02ebfbe675268a6ed4eac403
Hans Elde <hanserik.elde@gmail.com>
2022-12-13 12:21:09 -0800

    menu integrated, fully working, some old code cleaned up

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,32 +1,20 @@
 /* global document, window, hljs, $ */
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const skoposWhite = '#FFFFFF';
   const skoposOrange = '#E28027';
   const skoposBlue = '#2E939E';
   const skoposRed = '#D3593D'
-  // const skoposLogo = document.querySelector('#skopos-logo');
-  const nav = document.querySelector('nav');
-  const navLinks = document.querySelectorAll('#site-navigation a');
   const main = document.querySelector('main');
-  const header = document.querySelector('header');
   const ourTeam = document.querySelector('#our-team');
-  const homeLink = document.querySelector('#home-link');
   const $caseStudyNav = $('#case-study nav');
   const caseStudyNav = document.querySelector('#case-study nav');
   const $sideNavLogo = $('#side-nav');
-  const caseStudyLink = document.querySelector('#case-study-link');
-  const ourTeamLink = document.querySelector('#our-team-link');
   const caseStudyNavUl = document.querySelector('#case-study nav ul');
-  const mobileCaseStudyNavUl = document.querySelector('#case-study-mobile ul');
+  const mobileCaseStudyNavUl = document.querySelector('#case-study-mobile');
   const $toTop = $('#toTop-logo');
 
-  // window.onresize = () => {
-  //   if (window.innerWidth < 1200) absPosition(headerLogo);
-  // }
-
-  const hamburgerImg = document.querySelector('#hamburger img');
+  const hamburgerImg = document.querySelector('#hamburger svg');
   const hamburger = document.querySelector('#hamburger');
   const xButton = document.querySelector('#x');
   const menu = document.querySelector('.menu');
@@ -34,15 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggleExpand = (element) => element.classList.toggle('expand');
   const hide = (element) => element.classList.add('hidden');
   const show = (element) => element.classList.remove('hidden');
-  // const fixPosition = (element) => element.style.position = 'fixed';
-  // const absPosition = (element) => element.style.position = 'absolute';
 
   hamburger.onclick = (e) => {
     hide(hamburgerImg);
     toggleExpand(hamburger);
     show(xButton);
     setTimeout(() => {
-      // fixPosition(headerLogo);
       toggleExpand(hamburger);
       show(menu)
     }, 250);
@@ -62,7 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const xButton = e.currentTarget;
     hide(xButton);
     hide(menu);
-    // if (window.innerWidth < 1200) absPosition(headerLogo);
     show(hamburgerImg);
   }
 
@@ -76,17 +60,9 @@ document.addEventListener('DOMContentLoaded', () => {
     );
   });
 
-  let topNavVisible = false;
-  let smallNavVisible = false;
   const getScrollPosition = () => window.scrollY;
-  let scrollPosition = getScrollPosition();
   const getWindowHeight = () => window.innerHeight;
   const getWindowWidth = () => window.innerWidth;
-  const isNarrowScreen = () => getWindowWidth() < 1100;
-
-  const logos = [...document.querySelectorAll('.logo-links img')]
-    .filter((logo) => !/SKOPOS/.test(logo.id))
-    .map((logo) => logo.id.split('-')[0]);
 
   const snakeCaseify = (text) => text.toLowerCase().split(/ +|\//).join('-').replace(/\.|-{2,}/g, '-');
 
@@ -140,7 +116,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   (function () {
     let lastH2Id;
-    let lastH3Id;
 
     linkableHeaders.forEach((headerObj) => {
       if (headerObj.type === 'H2') {
@@ -186,57 +161,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }());
 
-  const changeImgSrc = (tag, url) => {
-    document.querySelector(`#${tag}`).src = url;
-  };
-
-  const logoUrls = {
-    githubWhite: './images/logos/github_white.png',
-    githubBlack: './images/logos/github_black.png',
-  };
-
-  const isOnHeader = (logo) => {
-    const position = getScrollPosition();
-    const logoSelector = `#${logo}-logo`;
-    const logoElement = document.querySelector(logoSelector);
-    const logoHeight = logoElement.height;
-    const logoBottom = +window.getComputedStyle(logoElement).bottom.replace('px', '');
-    const logoOffset = logoHeight + logoBottom;
-    return position < logoOffset;
-  };
-
-  const isOnTeamSection = (logo) => {
-    const position = getScrollPosition();
-    const ourTeamOffset = ourTeam.getBoundingClientRect().top;
-    const ourTeamPosition = position + ourTeamOffset;
-    const logoSelector = `#${logo}-logo`;
-    const logoElement = document.querySelector(logoSelector);
-    const logoHeight = logoElement.height;
-    const logoBottom = +window.getComputedStyle(logoElement).bottom.replace('px', '');
-    const logoOffset = logoHeight + logoBottom;
-    const logoPosition = position + getWindowHeight() - logoOffset;
-    return logoPosition > ourTeamPosition;
-  };
-
-  const changeLogoColors = () => {
-    logos.forEach((logo) => {
-      const onHeader = isOnHeader(logo);
-      const onTeam = isOnTeamSection(logo);
-
-      if (onTeam) {
-        changeImgSrc(`${logo}-logo`, logoUrls[`${logo}Black`]);
-      } else if (onHeader) {
-        changeImgSrc(`${logo}-logo`, logoUrls[`${logo}Black`]);
-      } else {
-        changeImgSrc(`${logo}-logo`, logoUrls[`${logo}Black`]);
-      }
-    });
-  };
-
   const handleCaseStudyNavStyles = () => {
     const positions = getCaseStudyHeadingPositions();
     const positionValues = Object.values(positions);
-    const positionSelectors = Object.keys(positions);
     const mobileCaseStudyNav = document.querySelector('#case-study-mobile');
     const position = getScrollPosition();
 
@@ -308,117 +235,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  // const styleNavColors = (bgColor, textColor, hoverColor) => {
-  //   // nav.style.backgroundColor = bgColor;
-  //   const links = Array.prototype.slice.call(navLinks).concat(mobileCaseStudyLinks);
-  //   links.forEach((link) => {
-  //     link.style.color = textColor;
+  document.addEventListener('scroll', () => handleCaseStudyNav());
 
-  //     link.addEventListener('mouseenter', () => {
-  //       link.style.color = hoverColor;
-  //     });
-
-  //     link.addEventListener('mouseleave', () => {
-  //       link.style.color = textColor;
-  //     });
-  //   });
-  // };
-
-  // const handleNavColors = () => {
-  //   const onHeader = isOnHeader('skopos');
-  //   const onTeam = isOnTeamSection('skopos');
-  //   const onMain = !(onHeader || onTeam);
-  //   const isWideScreen = !isNarrowScreen();
-  //   const isSkopos = true;
-  //   if (true) {
-  //     if (isWideScreen && !onMain && topNavVisible) {
-  //       styleNavColors(skoposWhite, skoposRed, skoposBlue);
-  //       changeImgSrc('skopos-logo', 'images/logos/SKOPOS_logo_color.png'); // black
-  //     } else {
-  //       styleNavColors(skoposWhite, skoposRed, skoposBlue);
-  //       changeImgSrc('skopos-logo', 'images/logos/SKOPOS_logo_color.png');
-  //     }
-  //   }
-  // };
-
-  // const showNav = () => {
-  //   const position = getScrollPosition();
-  //   const narrowScreen = isNarrowScreen();
-  //   topNavVisible = true;
-
-  //   handleNavColors();
-  //   scrollPosition = position;
-
-  //   if (narrowScreen) document.body.style.backgroundColor = '#282828';
-  //   $(nav).slideDown('fast');
-  // };
-
-  // const showSite = () => {
-  //   const siteElements = [header, main, ourTeam, document.body];
-  //   // remove "display = 'none'" set when small nav was displayed
-  //   siteElements.forEach((el) => el.removeAttribute('style'));
-  // };
-
-  // const hideSite = () => {
-  //   header.style.display = 'none';
-  //   main.style.display = 'none';
-  //   ourTeam.style.display = 'none';
-  // };
-
-  // const hideNav = () => {
-  //   smallNavVisible = false;
-  //   topNavVisible = false;
-  //   handleNavColors();
-  //   $(nav).slideUp('fast');
-  //   showSite();
-  // };
-
-  // const toggleNav = () => {
-  //   if (smallNavVisible) {
-  //     hideNav();
-  //     window.scrollTo(0, scrollPosition);
-  //   } else {
-  //     showNav();
-  //     smallNavVisible = true;
-  //     hideSite();
-  //   }
-  // };
-
-  // const handleNavDisplay = (e) => {
-  //   console.log("hi")
-  //   if (isNarrowScreen()) {
-  //     toggleNav();
-  //   } else {
-  //     showNav();
-  //   }
-  // };
-
-  // skoposLogo.addEventListener('click', handleNavDisplay);
-  // main.addEventListener('mouseenter', hideNav);
-  // ourTeam.addEventListener('mouseenter', hideNav);
-  // header.addEventListener('mouseenter', hideNav);
-  // homeLink.addEventListener('click', hideNav);
-  // caseStudyLink.addEventListener('click', hideNav);
-  // mobileCaseStudyLinks.forEach((link) => link.addEventListener('click', hideNav));
-  // ourTeamLink.addEventListener('click', hideNav);
-
-  document.addEventListener('scroll', () => {
-    if (!smallNavVisible) {
-      // changeLogoColors();
-      handleCaseStudyNav();
-    }
-    // handleNavColors();
-  });
-
-  window.addEventListener('resize', () => {
-    handleCaseStudyNav();
-    // handleNavColors();
-    if (!isNarrowScreen() && smallNavVisible) {
-      showSite();
-      hideNav();
-      window.scrollTo(0, scrollPosition);
-    }
-  });
+  window.addEventListener('resize', () => handleCaseStudyNav());
 
   caseStudyNav.addEventListener('click', (e) => {
     if (e.target.tagName === 'A') {
